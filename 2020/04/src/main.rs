609eed7255986dad36ff3d2f4ef39528a010dd4a
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2020-12-15 01:37:49 -0800

    [WIP]: parse hair, eye color, improve tests

--- 2020/04/src/main.rs
+++ 2020/04/src/main.rs
@@ -137,6 +137,30 @@ impl Passport {
         Err(PassportError::InvalidHeight)
     }
 
+    fn parse_hair_color(hair_color: &str) -> Result<String, PassportError> {
+        match (hair_color.len(), hair_color.starts_with('#')) {
+            (7, true) => {
+                u32::from_str_radix(&hair_color[1..], 16)
+                    .map_err(|_| PassportError::InvalidHairColor)?;
+                Ok(hair_color.to_string())
+            }
+            _ => Err(PassportError::InvalidHairColor),
+        }
+    }
+
+    fn parse_eye_color(eye_color: &str) -> Result<EyeColor, PassportError> {
+        match eye_color {
+            "amb" => Ok(EyeColor::Amber),
+            "blu" => Ok(EyeColor::Blue),
+            "brn" => Ok(EyeColor::Brown),
+            "gry" => Ok(EyeColor::Gray),
+            "grn" => Ok(EyeColor::Green),
+            "hzl" => Ok(EyeColor::Hazel),
+            "oth" => Ok(EyeColor::Other),
+            _ => Err(PassportError::InvalidEyeColor),
+        }
+    }
+
     fn new(passport: &str) -> Result<Self, PassportError> {
         let fields = Self::fields(passport)?;
         println!("{:#?}", fields);
@@ -146,10 +170,9 @@ impl Passport {
         let issue_year = fields.get("iyr").ok_or(PassportError::MissingField)?;
         let expiration_year = fields.get("eyr").ok_or(PassportError::MissingField)?;
         let birth_year = fields.get("byr").ok_or(PassportError::MissingField)?;
-
         let height = fields.get("hgt").ok_or(PassportError::MissingField)?;
-        let _hair_color = fields.get("hcl").ok_or(PassportError::MissingField)?;
-        let _eye_color = fields.get("ecl").ok_or(PassportError::MissingField)?;
+        let hair_color = fields.get("hcl").ok_or(PassportError::MissingField)?;
+        let eye_color = fields.get("ecl").ok_or(PassportError::MissingField)?;
 
         Ok(Self {
             id: Self::parse_id(id)?,
@@ -158,8 +181,8 @@ impl Passport {
             expiration_year: Self::parse_expiration_year(expiration_year)?,
             birth_year: Self::parse_birth_year(birth_year)?,
             height: Self::parse_height(height)?,
-            hair_color: "#ffffff".to_string(),
-            eye_color: EyeColor::Hazel,
+            hair_color: Self::parse_hair_color(hair_color)?,
+            eye_color: Self::parse_eye_color(eye_color)?,
         })
     }
 }
@@ -171,19 +194,92 @@ fn main(_args: Args) -> anyhow::Result<()> {
 
 #[cfg(test)]
 mod test {
-    use super::Passport;
+    use super::{EyeColor, Height, Passport, PassportError};
 
     #[test]
-    fn test_valid_passport() {
+    fn test_valid_passports() {
         let valid_passports = vec![
-            "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f",
-            "eyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
-            "hcl:#888785\nhgt:164cm byr:2001 iyr:2015 cid:88\npid:545766238 ecl:hzl\neyr:2022",
-            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+            (
+                "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f",
+                Passport {
+                    id: "087499704".to_string(),
+                    country_id: None,
+                    issue_year: 2012,
+                    expiration_year: 2030,
+                    birth_year: 1980,
+                    height: Height::Inches(74),
+                    hair_color: "#623a2f".to_string(),
+                    eye_color: EyeColor::Green,
+                },
+            ),
+            (
+                "eyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+                Passport {
+                    id: "896056539".to_string(),
+                    country_id: Some("129".to_string()),
+                    issue_year: 2014,
+                    expiration_year: 2029,
+                    birth_year: 1989,
+                    height: Height::Centimeters(165),
+                    hair_color: "#a97842".to_string(),
+                    eye_color: EyeColor::Blue,
+                },
+            ),
+            (
+                "hcl:#888785\nhgt:164cm byr:2001 iyr:2015 cid:88\npid:545766238 ecl:hzl\neyr:2022",
+                Passport {
+                    id: "545766238".to_string(),
+                    country_id: Some("88".to_string()),
+                    issue_year: 2015,
+                    expiration_year: 2022,
+                    birth_year: 2001,
+                    height: Height::Centimeters(164),
+                    hair_color: "#888785".to_string(),
+                    eye_color: EyeColor::Hazel,
+                },
+            ),
+            (
+                "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+                Passport {
+                    id: "093154719".to_string(),
+                    country_id: None,
+                    issue_year: 2010,
+                    expiration_year: 2021,
+                    birth_year: 1944,
+                    height: Height::Centimeters(158),
+                    hair_color: "#b6652a".to_string(),
+                    eye_color: EyeColor::Blue,
+                },
+            ),
         ];
 
-        for passport in valid_passports {
-            assert!(Passport::new(passport).is_ok());
+        for (passport, expected) in valid_passports {
+            assert_eq!(Passport::new(passport).unwrap(), expected);
+        }
+    }
+
+    #[test]
+    fn test_invalid_passports() {
+        let invalid_passports = vec![
+            (
+                "eyr:1972 cid:100\nhcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+                Err(PassportError::InvalidID),
+            ),
+            (
+                "iyr:2019\nhcl:#602927 eyr:1967 hgt:170cm\necl:grn pid:012533040 byr:1946",
+                Err(PassportError::InvalidExpirationYear),
+            ),
+            (
+                "hcl:dab227 iyr:2012\necl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+                Err(PassportError::InvalidHairColor),
+            ),
+            (
+                "hgt:59cm ecl:zzz\neyr:2038 hcl:74454a iyr:2023\npid:3556412378 byr:2007",
+                Err(PassportError::InvalidID),
+            ),
+        ];
+        for (passport, expected) in invalid_passports {
+            assert_eq!(Passport::new(passport), expected);
         }
     }
 }
